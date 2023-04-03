06f545e05c78f357a2be856bac6b3c67d9196c10
a-s-fernando <fernandoalsth@gmail.com>
2023-04-03 01:16:23 +0100

    add some extra endpoints

--- index.js
+++ index.js
@@ -8,15 +8,71 @@ app.get("/", (req, res) => {
 });
 
 app.get("/characters", async (req, res) => {
-  console.log("endpoint hit");
   const data =
-    await client.query(`SELECT character.characterID, character.name, dob, personality, appearance, background,
-  gender.name as gender, race.name as race, ethnicity, STRING_AGG(source.name,', ') as source FROM character
-  FULL OUTER JOIN gender ON gender.genderID = character.genderID
-  FULL OUTER JOIN race ON race.raceID = character.raceID
-  FULL OUTER JOIN character_source ON character_source.characterID = character.characterID
-  FULL OUTER JOIN source ON character_source.sourceID = source.sourceID
-  GROUP BY character.characterID, gender.name, race.name;`);
+    await client.query(`SELECT data.*, STRING_AGG(character_dislikes.name,', ') as dislikes FROM
+  (SELECT data.*, STRING_AGG(character_likes.name,', ') as likes FROM data
+  JOIN character_likes ON character_likes.characterID = data.id
+  GROUP BY data.id, data.name, data.dob, data.personality,
+  data.appearance, data.background, data.gender, data.race, data.ethnicity, data.source) as data
+  JOIN character_dislikes ON character_dislikes.characterID = data.id
+  GROUP BY data.id, data.name, data.dob, data.personality,
+  data.appearance, data.background, data.gender, data.race, data.ethnicity, data.source, data.likes
+  ORDER BY id ASC`);
+  console.log(data.rows);
+  res.send(data.rows);
+});
+
+app.get("/characters/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const data = await client.query(
+    `SELECT data.*, STRING_AGG(character_dislikes.name,', ') as dislikes FROM
+    (SELECT data.*, STRING_AGG(character_likes.name,', ') as likes FROM data
+    JOIN character_likes ON character_likes.characterID = data.id
+    GROUP BY data.id, data.name, data.dob, data.personality,
+    data.appearance, data.background, data.gender, data.race, data.ethnicity, data.source) as data
+    JOIN character_dislikes ON character_dislikes.characterID = data.id
+    WHERE data.id = $1
+    GROUP BY data.id, data.name, data.dob, data.personality,
+    data.appearance, data.background, data.gender, data.race, data.ethnicity, data.source, data.likes
+    ORDER BY id ASC;`,
+    [id]
+  );
+  console.log(data.rows);
+  res.send(data.rows);
+});
+
+app.get("/characters/:id/relations", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const data = await client.query(
+    `SELECT character.characterID, character.name, character_relation.relationship as relationship FROM character
+  JOIN character_relation ON character_relation.targetID = character.characterID
+  WHERE character_relation.characterID = $1;`,
+    [id]
+  );
+  console.log(data.rows);
+  res.send(data.rows);
+});
+
+app.get("/characters/:id/likes", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const data = await client.query(
+    `SELECT character_likes.name as name FROM character_likes
+  JOIN character ON character.characterID = character_likes.characterID
+  WHERE character.characterID = $1;`,
+    [id]
+  );
+  console.log(data.rows);
+  res.send(data.rows);
+});
+
+app.get("/characters/:id/dislikes", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const data = await client.query(
+    `SELECT character_dislikes.name as name FROM character_dislikes
+  JOIN character ON character.characterID = character_dislikes.characterID
+  WHERE character.characterID = $1;`,
+    [id]
+  );
   console.log(data.rows);
   res.send(data.rows);
 });
