3c99221e3ee82a3561d663d07926d1f4c5f63a8b
a-s-fernando <fernandoalsth@gmail.com>
2023-04-06 20:27:05 +0100

    error handle read + refactor

--- CRUD/read.js
+++ CRUD/read.js
@@ -5,76 +5,73 @@ async function getAll(req, res) {
   console.log(data.rows);
   res.json(data.rows);
 }
+async function getFromID(req, res, query) {
+  try {
+    const id = parseInt(req.params.id);
+    const data = await client.query(query, [id]);
+    console.log(data.rows);
+    res.send(data.rows);
+  } catch (err) {
+    res.sendStatus(404);
+  }
+}
 
 async function getOne(req, res) {
-  const id = parseInt(req.params.id);
-  const data = await client.query(
+  getFromID(
+    req,
+    res,
     `SELECT * FROM complete_data
-	  WHERE complete_data.id = $1`,
-    [id]
+	  WHERE complete_data.id = $1;`
   );
-  console.log(data.rows);
-  res.send(data.rows);
 }
 
 async function getRelations(req, res) {
-  const id = parseInt(req.params.id);
-  const data = await client.query(
+  getFromID(
+    req,
+    res,
     `SELECT character.characterID, character.name, character_relation.relationship as relationship FROM character
   JOIN character_relation ON character_relation.targetID = character.characterID
-  WHERE character_relation.characterID = $1;`,
-    [id]
+  WHERE character_relation.characterID = $1;`
   );
-  console.log(data.rows);
-  res.send(data.rows);
 }
 
 async function getLikes(req, res) {
-  const id = parseInt(req.params.id);
-  const data = await client.query(
+  getFromID(
+    req,
+    res,
     `SELECT character_likes.name as name FROM character_likes
   JOIN character ON character.characterID = character_likes.characterID
-  WHERE character.characterID = $1;`,
-    [id]
+  WHERE character.characterID = $1;`
   );
-  console.log(data.rows);
-  res.send(data.rows);
 }
 
 async function getDislikes(req, res) {
-  const id = parseInt(req.params.id);
-  const data = await client.query(
+  getFromID(
+    req,
+    res,
     `SELECT character_dislikes.name as name FROM character_dislikes
   JOIN character ON character.characterID = character_dislikes.characterID
-  WHERE character.characterID = $1;`,
-    [id]
+  WHERE character.characterID = $1;`
   );
-  console.log(data.rows);
-  res.send(data.rows);
 }
 
 async function getProfile(req, res) {
-  const id = parseInt(req.params.id);
-  const data = await client.query(
-    `SELECT character_dislikes.name as name FROM character_dislikes
-  JOIN character ON character.characterID = character_dislikes.characterID
-  WHERE character.characterID = $1;`,
-    [id]
+  getFromID(
+    req,
+    res,
+    `SELECT image.imageURL as profile FROM image
+  JOIN character_profile_image ON character_profile_image.imageID = image.imageID
+  WHERE character_profile_image.characterID = $1;`
   );
-  console.log(data.rows);
-  res.send(data.rows);
 }
 
 async function getImages(req, res) {
-  const id = parseInt(req.params.id);
-  const data = await client.query(
-    `SELECT character_dislikes.name as name FROM character_dislikes
-  JOIN character ON character.characterID = character_dislikes.characterID
-  WHERE character.characterID = $1;`,
-    [id]
+  getFromID(
+    req,
+    res,
+    `SELECT images FROM complete_data
+  WHERE id = $1;`
   );
-  console.log(data.rows);
-  res.send(data.rows);
 }
 
 module.exports = {
