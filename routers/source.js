8ba6d7242b9820f4d1caa3a6a59d1831be490c03
Alain Sidath Fernando <fernandoalsth@gmail.com>
2023-06-05 17:02:17 +0100

    Convert source handling to use the MVC pattern

--- routers/source.js
+++ routers/source.js
@@ -1,15 +1,17 @@
 const { Router } = require("express");
 
-const { getAllSources } = require("../CRUD/read");
-const { deleteSource } = require("../CRUD/delete");
-const { createSource } = require("../CRUD/create");
-const { updateSource } = require("../CRUD/update");
+const {
+  sourceIndex,
+  deleteSource,
+  create,
+  update,
+} = require("../controllers/source");
 
 const sourceRouter = Router();
 
-sourceRouter.get("/", getAllSources);
+sourceRouter.get("/", sourceIndex);
 sourceRouter.delete("/", deleteSource);
-sourceRouter.post("/", createSource);
-sourceRouter.put("/", updateSource);
+sourceRouter.post("/", create);
+sourceRouter.put("/", update);
 
 module.exports = sourceRouter;
